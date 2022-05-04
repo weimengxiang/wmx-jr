2cc091a4721fb6b550ec23c0553ab464c13ae6f5
2636968639@qq.com <Qq14796213925>
2022-05-04 14:45:23 +0800

    Changes

---
+++ wmx-jr-eureka/src/test/java/com/wmx/jr/WmxJrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmx.jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmxJrEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
